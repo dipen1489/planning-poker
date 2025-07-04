b77dd7b0837163b5e20404567ec35e9bcd74d00d
dipen1489 <dipen1489@gmail.com>
2025-07-04 12:10:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,26 +11,66 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 let votes = {};
 let users = {};
+let revealed = false;
+
+const isSameUser = (newName) => {
+    for(let user in users){
+        if(users[user].name.toLowerCase() === newName.toLowerCase()) {
+            return true
+        }
+    }
+    return false
+}
 
 io.on('connection', (socket) => {
   socket.on('join', (name) => {
-    socket.name = name; // Assign the name to the socket
-    users[socket.id] = name;
+    if(!isSameUser(name)) {
+        socket.name = name; // Assign the name to the socket
+        users[socket.id] = { name, hasVoted: false };
+    }
     io.emit('updateUsers', Object.values(users));
+    if (revealed) {
+      io.emit('revealVotes', votes);
+    }
   });
 
   socket.on('vote', ({ name, value }) => {
-    votes[name] = value;
-    io.emit('updateVotes', votes);
+    if (users[socket.id]) {
+        users[socket.id].hasVoted = true;
+        votes[name] = value;
+        io.emit('updateVotes', votes);
+        io.emit('updateUsers', Object.values(users));
+    }
+    if (revealed) {
+        io.emit('revealVotes', votes);
+    }
   });
 
   socket.on('revealVotes', () => {
+    revealed = true;
     io.emit('revealVotes', votes);
   });
 
   socket.on('reset', () => {
     votes = {};
+    revealed = false; // Reset the revealed flag
+    for(let user in users){
+        users[user].hasVoted = false
+    }
     io.emit('reset');
+    io.emit('updateUsers', Object.values(users));
+  });
+
+  socket.on('removeParticipant', (name) => {
+    const userSocketId = Object.keys(users).find(id => users[id].name === name);
+    if (userSocketId) {
+      delete users[userSocketId];
+      io.emit('updateUsers', Object.values(users));
+      if (revealed) {
+        io.emit('revealVotes', votes);
+      }
+      io.emit('removeUser', name);
+    }
   });
 
   socket.on('disconnect', () => {
@@ -41,7 +81,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
